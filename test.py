b7134154184e8b0b44afd5ca2b7eb2efdd8a051d
Devil_king <mdrifathasan2569@gmail.com>
2026-06-14 06:02:43 +0600

    Update test.py

--- test.py
+++ test.py
@@ -344,10 +344,10 @@ from os import system as cmd
 from concurrent.futures import ThreadPoolExecutor as tred
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
 os.system('clear')
-    print(f'\x1b[38;5;160m[\033[1;37m√\x1b[38;5;160m]\x1b[38;5;46m installing site package please wait ...\n')
-    time.sleep(5)
-    os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-    
+print(f'\x1b[38;5;160m[\033[1;37m√\x1b[38;5;160m]\x1b[38;5;46m installing site package please wait ...\n')
+time.sleep(5)
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+
 try:
      import requests 
 except ImportError:
